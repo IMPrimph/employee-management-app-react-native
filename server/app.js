e71b16767157ca34c718651a0848de370163de32
IMPrimph <vishnugajulapalli123@gmail.com>
2021-05-13 14:54:18 +0530

    create schema and connect to mongoose

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const Employee = require('./Employee')
 
 const app = express()
 
 const DB_URL = 'mongodb+srv://primph:noobie@cluster0.n57br.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB Connected")
+})
 
 app.get('/', (req, res) => {
     res.send('Hi')
